9c8c82619c13a823ae55a81daf020218140dff5d
Nyenty Ayuk <ayuknyenty15@gmail.com>
2023-04-17 18:05:26 -0400

    Addition of Javascript to actually display the range calculation without loading a new page. this required minor form validation as well as setting default values to be able to properly work. (Will look in to better implementation later)

--- flask_calc.py
+++ flask_calc.py
@@ -1,3 +1,5 @@
+import json
+
 from flask import Flask, render_template, request,session
 import weaponformulas
 
@@ -6,7 +8,7 @@ app= Flask(__name__)
 """index page"""
 @app.route("/", methods = ["GET", "POST"])
 def index():
-
+    range = {}
     if request.method == "POST":
         web_str = int(request.form.get("str"))
         web_dex = int(request.form.get("dex"))
@@ -17,5 +19,6 @@ def index():
         castella = weaponformulas.Weaponformula(web_str, web_dex, web_int,
                                                 web_luk, web_wpattk,
                                                 web_wptype)
-        return castella.weapon_calc()
-    return render_template('index.html')
+        range = {"Range": castella.weapon_calc()}
+    calculation = json.dumps(range)
+    return render_template('index.html', calculation=calculation)
