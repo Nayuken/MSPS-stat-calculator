e0f6248a55b78730f426413bc051acef6d6fa2ea
Nyenty Ayuk <ayuknyenty15@gmail.com>
2023-04-18 18:03:03 -0400

    Code clean up and starting to build the skeleton of the magic attack calculator for the site. Will take some time since I'll be doing a few different versions.

--- flask_calc.py
+++ flask_calc.py
@@ -16,9 +16,15 @@ def index():
         web_luk = int(request.form.get("luk"))
         web_wpattk = int(request.form.get("wp_attk"))
         web_wptype = str(request.form.get("weapon_type"))
+
         castella = weaponformulas.Weaponformula(web_str, web_dex, web_int,
                                                 web_luk, web_wpattk,
                                                 web_wptype)
         range = {"Range": castella.weapon_calc()}
     calculation = json.dumps(range)
     return render_template('index.html', calculation=calculation)
+
+
+@app.route("/spells", methods = ["GET", "POST"])
+def spells():
+    return render_template('spells.html')
